41aac92ae00d59a2d897eb4a533dfe14db0d3a5d
peterwitait <58208690+peterwitait@users.noreply.github.com>
2021-05-24 18:42:36 +0200

    Add files via upload

--- 04Animation/scripts/main.js
+++ 04Animation/scripts/main.js
@@ -5,12 +5,12 @@ var rotationSpeed = 0.04;
 var myOtherBox = document.getElementById('myOtherBox');
 
 function spin(){
-myOtherBox.object3D.rotation.x += rotationSpeed;
-console.log(myOtherBox.object3D.rotation.x);
 myOtherBox.object3D.rotation.y += rotationSpeed;
 console.log(myOtherBox.object3D.rotation.y);
 myOtherBox.object3D.rotation.z += rotationSpeed;
 console.log(myOtherBox.object3D.rotation.z);
+myOtherBox.object3D.rotation.x = rotationSpeed;
+console.log(myOtherBox.object3D.rotation.x);
 }
 
-setInterval(spin, 16); //equivalent to 60 fps
+setInterval(spin, 2); //equivalent to 60 fps
